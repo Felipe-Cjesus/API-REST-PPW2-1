af44fdd793adc365f0687fbc25b1b45f5842a15a
pventur-a <6507086-pventur-a@users.noreply.gitlab.com>
2021-06-30 05:38:55 -0400

    add a rota / com as informações do endpoints

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const PORT = process.env.PORT || 8000;
 const routes = require("./src/routes");
 
 app.use("/api", routes);
+app.get('/', (req, res)=>{
+  res.json({
+    "name":"Pedro Ventura",
+    "endpoint":["api/cursos", "api/universidade", "api/clube", "api/estadio"],
+    "description":""
+  })
+})
 app.use(function (err, req, res, next) {
   if (res.statusCode == 200) {
     res.statusCode = 500;
